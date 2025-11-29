c6a92d9afed2de8fd00b2bc69b82e9d5e4113967
Zapip <zafif3165@gmail.com>
2025-11-29 19:55:08 +0700

    fix(deploy):umkm card to Jelajah Dukuh

--- src/components/umkm-card.tsx
+++ src/components/umkm-card.tsx
@@ -1,35 +1,36 @@
 import Image from "next/image";
-import { umkm } from "@/types/JelajahDukuh";
+import { JelajahDukuh } from "@/types/JelajahDukuh";
+import Link from "next/link";
 
-export default function UmkmCard({
+export default function JelajahCard({
     nama_usaha,
     deskripsi,
-    image,
+    image_url,
     link_maps,
     kategori_usaha,
-}: umkm) {
+}: JelajahDukuh) {
     return (
-        <div className="bg-white shadow-md rounded-2xl overflow-hidden hover:shadow-lg transition p-4">
+        <article className="bg-white shadow-md rounded-2xl overflow-hidden hover:shadow-lg transition p-4">
             <Image
-                src={image || "/placeholder.jpg"}
+                src={image_url || "/placeholder.jpg"}
                 alt={nama_usaha}
                 width={400}
                 height={250}
                 className="w-full h-48 object-cover rounded-lg"
             />
-            <div className="mt-3">
+            <section className="mt-3">
                 <h3 className="text-lg font-semibold text-gray-800">{nama_usaha}</h3>
                 <p className="text-gray-600 text-sm mt-2 line-clamp-3">{deskripsi}</p>
                 <p className="text-gray-600 text-sm mt-2 line-clamp-3">{kategori_usaha}</p>
-                <a
-                    href={link_maps}
+                <Link
+                    href={link_maps || "#"}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="inline-block mt-3 bg-green-600 text-white text-sm px-4 py-2 rounded-lg hover:bg-green-700 transition"
                 >
                     Lihat di Maps
-                </a>
-            </div>
-        </div>
+                </Link>
+            </section>
+        </article>
     );
 }
